579b4ef3e24eed9d2fdb3ac40c5fc195b3c819ff
danielzarazua <danielzarazuaflores@hotmail.com>
2019-04-22 22:42:22 -0500

    UPDATE: nuevo archivo funciones

---
+++ funciones.php
@@ -0,0 +1,15 @@
+<?php
+echo "funcion que convierte de millas a kilometros";
+
+
+function miki ($k)
+
+	{
+	$conversor = $k * 1.60934;
+	echo "El resultado es:", $conversor;
+	}	
+
+	$conversor
+
+
+?>
